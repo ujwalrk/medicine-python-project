b13a518f0e5f8135bdc264be723c2859a2a60a87
Ubuntu <ubuntu@ip-172-31-34-223.ap-south-1.compute.internal>
2023-10-19 10:11:00 +0000

    Python Medicine Store App Updated

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__, template_folder="templates")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:5ubhanuRapp1d!@localhost/medicine_stores'
+db = SQLAlchemy(app)
 
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "<enter your password>" #password
-app.config['MYSQL_DB'] = "medicine_stores"
-
-mysql = MySQL(app)
+class customers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(255))
+    mobile_no = db.Column(db.String(20))
+    age = db.Column(db.Integer)
+    address = db.Column(db.String(255))
+    prescription = db.Column(db.Text)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -17,14 +21,19 @@ def index():
         age = request.form["age"]
         address = request.form["address"]
         prescription = request.form["prescription"]
-		
-        cur = mysql.connection.cursor()
 
-        cur.execute( "INSERT INTO customers (full_name,mobile_no, age, address, prescription) VALUES(%s, %s, %s, %s, %s)",(full_name,mobile_no, age, address, prescription))
-        mysql.connection.commit()
-        cur.close()
-        return render_template('success.html')
+        new_customers = customers(full_name=full_name, mobile_no=mobile_no, age=age, address=address, prescription=prescription)
+
+        try:
+            db.session.add(new_customers)
+            db.session.commit()
+            return render_template('success.html')
+        except Exception as e:
+            db.session.rollback()
+            return f"An error occurred: {str(e)}"
+
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
